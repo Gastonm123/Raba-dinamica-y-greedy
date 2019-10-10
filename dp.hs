2f08db7565ca1399bb29d2bca0d861fef647fd9d
Gaston Martinez Castro <gastonmartinezcastro@gmail.com>
2019-10-10 18:22:02 -0300

    Aplico stash

--- dp.hs
+++ dp.hs
@@ -4,33 +4,34 @@ maximo_subpalindromo string = foldr max 0 resultados
         longitud = (length string) - 1
 
         substrings = [(y,x) | y <- [0..longitud], x <- [(y+1)..longitud]]
-        
+
         insertar :: Int -> Int -> Int -> [[Int]] -> [[Int]]
-        insertar x y value matriz = [if i == x then insertar_row column else column | (i, column) <- zip [0..] matriz]
+        insertar x y value matriz = [if i == x then insertar_col column else column | (i, column) <- zip [0..] matriz]
             where
-                insertar_row column = [if j == y then value else element | (j, element) <- zip [0..] column]
-        
+                insertar_col column = [if j == y then value else element | (j, element) <- zip [0..] column]
+
+        int :: Bool -> Int
         int True = 1
         int False = 0
 
-        pali :: Int -> Int -> [[Int]] -> ([[Int]], Int)       
+        pali :: Int -> Int -> [[Int]] -> ([[Int]], Int)
         pali x y dp = if value == 1
             then (dp1, int (string!!x == string!!y))
             else (dp1, 0)
-            where 
+            where
                 (dp1, value) = palindromo (x+1) (y-1) dp
-        
-        palindromo :: Int -> Int -> [[Int]] -> ([[Int]], Int)       
+
+        palindromo :: Int -> Int -> [[Int]] -> ([[Int]], Int)
         palindromo x y dp = if dp!!x!!y /= -1
             then (dp, dp!!x!!y)
             else if x == y
                 then (insertar x y 1 dp, 1)
                 else (insertar x y value dp1, value)
                     where
-                        (dp1, value) = if x == (y-1) 
-                            then (dp, int (string!!x == string!!y)) 
+                        (dp1, value) = if x == (y-1)
+                            then (dp, int (string!!x == string!!y))
                             else pali x y dp
-        
+
         largoSubstring :: (Int, Int) -> [[Int]] -> ([[Int]], Int)
         largoSubstring (x, y) dp = if value == 1
             then (dp1, y-x+1)
