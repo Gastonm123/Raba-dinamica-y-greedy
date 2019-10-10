bbcb95b6755f311dd17337b99f5b65f9c76951ac
Gaston Martinez Castro <gastonmartinezcastro@gmail.com>
2019-10-10 20:34:01 -0300

    Memes

--- dp.hs
+++ dp.hs
@@ -17,25 +17,4 @@ maximo_subpalindromo string = foldr max 0 largos
                 else pali x y
         
         largoSubstring (x, y) = if palindromo x y then (y-x+1) else 0
-        largos = map cantidad substrings
-
-
--- array = "adadada"
-
--- largo = (length array) - 1 
--- substrings = [(y,x) | y <- [0..largo], x <- [(y+1)..largo]]
-
--- cantidad (x, y) = if palindromo x y then (y-x) else 0
--- cantidades = map cantidad substrings
-
--- aplanado = [x | y <- cantidades, x <- y]
-
--- pali x y = if palindromo (x+1) (y-1)
---     then (array!!x == array!!y)
---     else False
-
--- palindromo x y = if x == y
---     then True
---     else if x == (y-1)
---         then (array!!x == array!!y)
---         else pali x y
+        largos = map largoSubstring substrings
