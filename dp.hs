f3448e347f1cf047b0f1fba991d4a1ab1b72b64e
Gaston Martinez Castro <gastonmartinezcastro@gmail.com>
2019-10-15 22:07:21 -0300

    Un capo el raba

--- dp.hs
+++ dp.hs
@@ -1,19 +1,42 @@
+import Data.Matrix
+
 maximo_subpalindromo :: [Char] -> Int
-maximo_subpalindromo string = foldr max 0 largos
+maximo_subpalindromo string = foldr max 0 resultados
     where
         longitud = (length string) - 1
 
         substrings = [(x,y) | x <- [0..longitud], y <- [x..longitud]]
         
-        pali x y = if palindromo (x+1) (y-1)
-            then (string!!x == string!!y)
-            else False
+        compare x y = if string!!x == string!!y then 1 else -1
+
+        pali :: Matrix Integer -> Int -> Int -> Matrix Integer
+        pali dp x y = if dp!(x+1, y+1) /= 0
+            then dp
+            else setElem value (x+1, y+1) dpRecursion
+                where
+                    dpRecursion = palindromo dp (x+1) (y-1)
+                    value = if dpRecursion!(x+2, y) == 1 
+                        then compare x y
+                        else -1
+
+        palindromo :: Matrix Integer -> Int -> Int -> Matrix Integer
+        palindromo dp x y = if dp!(x+1, y+1) /= 0
+            then dp
+            else if x == y
+                then setElem 1 (x+1, y+1) dp
+                else if x == (y-1)
+                    then setElem (compare x y) (x+1, y+1) dp
+                    else pali dp x y
+
+        calcularResultados :: Matrix Integer -> [(Int, Int)] -> [Int]
+        calcularResultados dp [] = []
+        calcularResultados dp (x:xs) = resultado : (calcularResultados dpRecursion xs)
+            where
+                (a, b) = x
+                dpRecursion = palindromo dp a b
+                resultado = if dpRecursion ! (a+1, b+1) == 1
+                    then (b-a+1)
+                    else 0
         
-        palindromo x y = if x == y
-            then True
-            else if x == (y-1)
-                then (string!!x == string!!y)
-                else pali x y
-        
-        largoSubstring (x, y) = if palindromo x y then (y-x+1) else 0
-        largos = map largoSubstring substrings
+        dp = zero (longitud + 1) (longitud + 1)
+        resultados = calcularResultados dp substrings
